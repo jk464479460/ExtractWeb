03e2427e397c8d18ef51aa7e35dbd2a11145f87a
jhon <464479460@qq.com>
2017-10-01 21:49:14 +0800

    exercise mathnet

---
+++ DataStatistics/Program.cs
@@ -0,0 +1,31 @@
+﻿using MathNet.Numerics.Statistics;
+using System;
+
+namespace DataStatistics
+{
+    public class DataProcess
+    {
+        public double GetMinium(double[] data)
+        {
+            return ArrayStatistics.Minimum(data);
+        }
+        public double GetMax(double[] data)
+        {
+            return ArrayStatistics.Maximum(data);
+        }
+        public double GetMean(double[] data)
+        {
+            return ArrayStatistics.Mean(data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new DataProcess();
+            var result =process.GetMinium(new double[] { 1,2,3,100});
+            Console.WriteLine(result);
+        }
+    }
+}
--- ExtractValueData/Program.cs
+++ ExtractValueData/Program.cs
@@ -1,36 +1,60 @@
-﻿using System;
-using System.Collections;
+﻿using DataStatistics;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
-using System.Text;
+using System.Runtime.InteropServices;
 
 namespace ExtractValueData
 {
-    class DayData
+    public class DayData
     {
         public string Date { get; set; }
-        public string Val { get; set; }
+        public double Val { get; set; }
     }
-    class Program
+
+    public class ExtractDayData
     {
-        static IList<DayData> daysData = new List<DayData>();
+        private string _code = string.Empty;
+        private string _url = string.Empty;
+        private string _queryUri = string.Empty;
 
-        static string queryUri="&code={code}&page={page}&per={per}&sdate={sdate}&edate={edate}&rt=0.24914203854989547";
-        static void Main(string[] args)
+        public IList<DayData> GetData(string sdate, string edate)
         {
-            var url = ConfigurationManager.AppSettings["Url"];
-            url = string.Format("{0}{1}",url,queryUri);
-
-            var code = "000977";
+            IList<DayData> daysData = new List<DayData>();
             var page = "1";
             var per = "1";
-            var sdate = "2017-08-01";
-            var edate = "2017-09-28";
 
-            url = url.Replace("{code}", code).Replace("{page}",page).Replace("{per}", per).Replace("{sdate}",sdate).Replace("{edate}", edate);
+            var pageRecordsInfo = GetPagesAndRecords(GetHtmBody(page,per, sdate, edate));
 
+            page = 1 + "";
+            per = pageRecordsInfo.Item1 + "";
+           
+            var res = GetHtmBody(page, per, sdate, edate);
+
+            var arr = res.Split(new string[] { "<tr>", "</tr>" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in arr)
+            {
+                var rowData = item.Split(new string[] { "<td>", "</td><td class='tor bold'>" }, StringSplitOptions.RemoveEmptyEntries);
+                if (rowData.Length >= 2)
+                    daysData.Add(new DayData { Date = rowData[0], Val = Convert.ToDouble(rowData[1]) });
+            }
+
+            return daysData;
+        }
+
+        public ExtractDayData(string url, string queryUri, string gjCode)
+        {
+            _code = gjCode;
+            _url = url;
+            _queryUri = queryUri;
+        }
+
+        private string DownLoadString(string url)
+        {
             WebClient wc = new WebClient();
             var st = wc.OpenRead(url);
             var sr = new StreamReader(st);
@@ -38,10 +62,23 @@ namespace ExtractValueData
             sr.Close();
             st.Close();
 
+            return res;
+        }
+
+        private string GetHtmBody(string page, string per, string sdate, string edate)
+        {
+            var url = string.Format("{0}{1}", _url, _queryUri);
+            url = url.Replace("{code}", _code).Replace("{page}", page).Replace("{per}", per).Replace("{sdate}", sdate).Replace("{edate}", edate);
+            var res = DownLoadString(url);
+            return res;
+        }
+
+        private Tuple<int, int> GetPagesAndRecords(string res)
+        {
             var records = 0;
             var pages = 0;
-            var arr = res.Split(new char[] { ','});
-            foreach(var item in arr)
+            var arr = res.Split(new char[] { ',' });
+            foreach (var item in arr)
             {
                 if (item.Contains("records"))
                 {
@@ -53,41 +90,61 @@ namespace ExtractValueData
                 }
             }
 
-            //for(var i = 1; i <= pages; i++)
+            return new Tuple<int, int>(records, pages);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
             {
-                url = ConfigurationManager.AppSettings["Url"]; ;
-                url = string.Format("{0}{1}", url, queryUri);
+                var url = ConfigurationManager.AppSettings["Url"];
+                var queryUri = ConfigurationManager.AppSettings["queryUri"];
+                var code = ConfigurationManager.AppSettings["GJCode"];
 
-                code = "000977";
-                page = 1+"";
-                per = records+"";
-              
+                var sdate = Console.ReadLine();
+                var edate = Console.ReadLine();
+                var daysData = new ExtractDayData(url, queryUri, code).GetData(sdate, edate);
+               
+                Console.WriteLine("data is ready>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                var dataProcess = new DataProcess();
+                var dataArr = daysData.Select(x => x.Val).ToArray();
+                var minium = dataProcess.GetMinium(dataArr);
+                var max = dataProcess.GetMax(dataArr);
+                var mean = dataProcess.GetMean(dataArr);
+                Console.WriteLine($"Max\tMin\tMean\n{max}\t{minium}\t{mean}");
 
-                url = url.Replace("{code}", code).Replace("{page}", page).Replace("{per}", per).Replace("{sdate}", sdate).Replace("{edate}", edate);
-                wc = new WebClient();
-                st = wc.OpenRead(url);
-                sr = new StreamReader(st);
-                res = sr.ReadToEnd();
-                sr.Close();
-                st.Close();
-
-                arr = res.Split(new string[] { "<tr>","</tr>"},StringSplitOptions.RemoveEmptyEntries);
-
-                foreach(var item in arr)
+                var maxPoints = TargetMaxMin(daysData, max);
+                Console.Write("Max:\n\t");
+                foreach(var item in maxPoints)
                 {
-                    var rowData = item.Split(new string[] { "<td>", "</td><td class='tor bold'>" }, StringSplitOptions.RemoveEmptyEntries);
-                    if(rowData.Length>=2)
-                    daysData.Add(new DayData { Date = rowData[0], Val = rowData[1] });
+                    Console.Write(item.Date+" ");
                 }
-            }
+                Console.Write("\n");
 
-            foreach(var item in daysData)
-            {
-                Console.WriteLine(item.Date+"\t"+item.Val);
-            }
-            Console.Read();
+                var minPoints = TargetMaxMin(daysData, minium);
+                Console.Write("Min:\n\t");
+                foreach (var item in minPoints)
+                {
+                    Console.Write(item.Date + " ");
+                }
+                Console.Write("\n");
+
+
+                Console.WriteLine("down >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            } while (Console.ReadLine() != "Q");
         }
 
-       
+       static IList<DayData> TargetMaxMin(IList<DayData> daysData, double findValue)
+       {
+            var result = new List<DayData>();
+            foreach(var item in daysData)
+            {
+                if (item.Val == findValue) result.Add(item);
+            }
+            return result;
+        }
     }
 }
