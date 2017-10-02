9a4dbc3ac22354375569d06fae6b809c0b257987
jhon <464479460@qq.com>
2017-10-02 21:22:50 +0800

    lljkjh

--- TopRankCodeList/Program.cs
+++ TopRankCodeList/Program.cs
@@ -1,51 +1,27 @@
-﻿using System;
+﻿using DataStatistics;
+using ExtractValueData;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Net;
+using System.Linq;
+using System.Threading;
 
 namespace TopRankCodeList
 {
-    public class TopResult
+    class ConsoleEx
     {
-        public string Code { get; set; }
-    }
-    public class TopRank
-    {
-        private readonly string _url;
-
-        public TopRank(string url)
+        public static void Write(bool file,string txt)
         {
-            _url = url;
-        }
-
-        public IList<TopResult> GetCodeList()
-        {
-            var result = new List<TopResult>();
-            var downLoadString = DownLoadString(_url);
-            var arr = downLoadString.Split(new string[] { "[", "]"},StringSplitOptions.RemoveEmptyEntries);
-            var codeArr = arr[1].Split(',');
-            foreach(var item in codeArr)
-            {
-                var itemArr = item.Split('|'); ;
-                result.Add(new TopResult { Code = itemArr[0]});
-            }
-            return result;
-        }
-
-        private string DownLoadString(string url)
-        {
-            WebClient wc = new WebClient();
-            var st = wc.OpenRead(url);
-            var sr = new StreamReader(st);
-            string res = sr.ReadToEnd();
-            sr.Close();
-            st.Close();
-
-            return res;
+            if (file)
+                using (var w = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt", true))
+                {
+                    w.Write(txt);
+                }
+            else
+                Console.Write(txt);
         }
     }
-
     class Program
     {
         static void Main(string[] args)
@@ -53,6 +29,54 @@ namespace TopRankCodeList
             var url = ConfigurationManager.AppSettings["Url"];
             var top = new TopRank(url);
             var codeList = top.GetCodeList();
+
+            var urlHistory = "http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz";
+
+            foreach(var code in codeList)
+            {
+                var queryUri = "&code={code}&page={page}&per={per}&sdate={sdate}&edate={edate}&rt=0."+ DateTime.Now.Second + "2038"+ DateTime.Now.Millisecond+ "498954"+DateTime.Now.Second;
+
+                var sdate = DateTime.Now.AddMonths(-1).ToString("yyyy-MM-dd");
+                var edate = DateTime.Now.ToString("yyyy-MM-dd") ;
+                var daysData = new ExtractDayData(urlHistory, queryUri, code.Code).GetData(sdate, edate);
+
+                var dataProcess = new DataProcess();
+                var dataArr = daysData.Select(x => x.Val).ToArray();
+                var minium = dataProcess.GetMinium(dataArr);
+                var max = dataProcess.GetMax(dataArr);
+                var mean = dataProcess.GetMean(dataArr);
+
+                ConsoleEx.Write(true, $"{code.Code} {code.Name}");
+                ConsoleEx.Write(true,$"Max\tMin\tMean\n{max}\t{minium}\t{mean}\n");
+
+                var maxPoints = TargetMaxMin(daysData, max);
+
+                ConsoleEx.Write(true,"Max:\n\t");
+                foreach (var item in maxPoints)
+                {
+                    ConsoleEx.Write(true,item.Date + " ");
+                }
+                ConsoleEx.Write(true,"\n");
+
+                var minPoints = TargetMaxMin(daysData, minium);
+                ConsoleEx.Write(true,"Min:\n\t");
+                foreach (var item in minPoints)
+                {
+                    ConsoleEx.Write(true,item.Date + " ");
+                }
+                ConsoleEx.Write(true,"\n");
+                Thread.Sleep(2000);
+            }
+        }
+
+        static IList<DayData> TargetMaxMin(IList<DayData> daysData, double findValue)
+        {
+            var result = new List<DayData>();
+            foreach (var item in daysData)
+            {
+                if (item.Val == findValue) result.Add(item);
+            }
+            return result;
         }
     }
 }
---
+++ TopRankCodeList/TopRank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace TopRankCodeList
+{
+    public class TopResult
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class TopRank
+    {
+        private readonly string _url;
+
+        public TopRank(string url)
+        {
+            _url = url;
+        }
+
+        public IList<TopResult> GetCodeList()
+        {
+            var result = new List<TopResult>();
+            var downLoadString = DownLoadString(_url);
+            var arr = downLoadString.Split(new string[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries);
+            var codeArr = arr[1].Split(',');
+            foreach (var item in codeArr)
+            {
+                var itemArr = item.Split('|');
+                if(itemArr.Length>=2)
+                    result.Add(new TopResult { Code = itemArr[0].Replace("\"",""),Name=itemArr[1] });
+            }
+            return result;
+        }
+
+        private string DownLoadString(string url)
+        {
+            WebClient wc = new WebClient();
+            var st = wc.OpenRead(url);
+            var sr = new StreamReader(st);
+            string res = sr.ReadToEnd();
+            sr.Close();
+            st.Close();
+
+            return res;
+        }
+    }
+}
